054f361942118232e7b1554ddce6a3a02458b7d3
uphong157 <uphong157@gmail.com>
2023-12-09 20:48:47 -0800

    Complete exercise 5.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,10 +13,28 @@ const App = () => {
     )  
   }, [])
 
+  // blog will generally have a user, but not technically guaranteed
+  // will filter out blogs that don't have an user
+  const blogsToShow = user === null
+    ? blogs
+    : blogs
+        .filter(blog => blog.user)
+        .filter(blog => blog.user.username === user.username)
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm setUser={setUser} />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
+      <p>{user.name} logged in</p>
+      {blogsToShow.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>

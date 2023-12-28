041783e7d64da872d4653acfc6eb0640a9570aa3
uphong157 <uphong157@gmail.com>
2023-12-27 21:48:26 -0800

    Complete exercise 5.7 and exercise 5.8 already completed

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [detailVisible, setDetailVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showIfDetailVisible = { display: detailVisible ? '' : 'none' }
+  const hideIfDetailVisible = { display: detailVisible ? 'none' : '' }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      </div>
+
+      <button 
+        style={hideIfDetailVisible}
+        onClick={() => setDetailVisible(true)}
+      >
+        view
+      </button>
+      <button 
+        style={showIfDetailVisible}
+        onClick={() => setDetailVisible(false)}
+      >
+        hide
+      </button>
+
+      <div style={showIfDetailVisible}>
+        {blog.url}<br/>
+        likes {blog.likes} <button>like</button><br/>
+        {blog.user.username}<br/>
+      </div>
+    </div>
+  )
+}
 
 export default Blog

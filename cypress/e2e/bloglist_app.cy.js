55d1b660cd5f809d98e005d716c21f6512576154
uphong157 <uphong157@gmail.com>
2024-02-10 15:41:03 -0800

    Complete exercise 5.18

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -14,4 +14,23 @@ describe('Blog app', () => {
   it('Login form is shown', () => {
     cy.contains('Log in to application')
   })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('input[name=Username]').type('Username')
+      cy.get('input[name=Password]').type('Password')
+      cy.contains('login').click()
+
+      cy.contains('FirstName LastName logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('input[name=Username]').type('Username')
+      cy.get('input[name=Password]').type('wrong password')
+      cy.contains('login').click()
+
+      cy.contains('Wrong credentials')
+      cy.contains('FirstName LastName logged in').should('not.exist')
+    })
+  })
 })

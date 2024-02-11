4445d5a09cf15592055d8f134081154ecc5705a9
uphong157 <uphong157@gmail.com>
2024-02-10 20:09:33 -0800

    Refactor for further exercises

--- cypress/e2e/bloglist_app.cy.js
+++ cypress/e2e/bloglist_app.cy.js
@@ -1,12 +1,13 @@
+const userA = {
+  name: 'FirstNameA LastNameA',
+  username: 'UsernameA',
+  password: 'PasswordA',
+}
+
 describe('Blog app', () => {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    const user = {
-      name: 'FirstName LastName',
-      username: 'Username',
-      password: 'Password',
-    }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    cy.request('POST', 'http://localhost:3003/api/users/', userA)
 
     cy.visit('http://localhost:5173')
   })
@@ -17,20 +18,20 @@ describe('Blog app', () => {
 
   describe('Login', function() {
     it('succeeds with correct credentials', function() {
-      cy.get('input[name=Username]').type('Username')
-      cy.get('input[name=Password]').type('Password')
+      cy.get('input[name=Username]').type(userA.username)
+      cy.get('input[name=Password]').type(userA.password)
       cy.contains('login').click()
 
-      cy.contains('FirstName LastName logged in')
+      cy.contains(`${userA.name} logged in`)
     })
 
     it('fails with wrong credentials', function() {
-      cy.get('input[name=Username]').type('Username')
+      cy.get('input[name=Username]').type(userA.username)
       cy.get('input[name=Password]').type('wrong password')
       cy.contains('login').click()
 
       cy.contains('Wrong credentials')
-      cy.contains('FirstName LastName logged in').should('not.exist')
+      cy.contains(`${userA.name} logged in`).should('not.exist')
     })
   })
 })
